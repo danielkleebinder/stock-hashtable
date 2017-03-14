013cf2915958e743d1b8b256bd4f90d2ada4f339
rupel190 <killakekz190@hotmail.com>
2017-03-14 16:50:56 +0100

    SingleStock implemented. Csv input data needs to be parsed beforehand.

--- src/data/SingleStock.java
+++ src/data/SingleStock.java
@@ -1,10 +1,28 @@
 package data;
 
+import java.util.Date;
+
 public class SingleStock {
-	private String abbreviation;
-	private String name;
+	private String abbreviation, name;
 	
-	//more data? 
+	private String date; //dateformattargh
+	private double open, high, low, close, adjClose;
+	private int volume;
 	
+	public SingleStock(String abbreviation, String name, String date, 
+			double open, double high, double low, double close, double adjClose,
+			int volume)
+	{
+		this.abbreviation = abbreviation;
+		this.name = name;
+		this.date = date;
+		this.open = open;
+		this.high = high;
+		this.low = low;
+		this.adjClose = adjClose;
+		this.volume = volume;
+	}
+	
+		
 	
 }
