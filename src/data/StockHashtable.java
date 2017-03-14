a3d99b12e465fee11279ad378e0a749117062c2e
Daniel Kleebinder <daniel.kleebinder@gmx.net>
2017-03-14 18:33:51 +0100

    added remove item methods for StockHashtable.java

--- src/data/StockHashtable.java
+++ src/data/StockHashtable.java
@@ -2,28 +2,24 @@ package data;
 
 import utils.StringUtils;
 
+/**
+ * Main stock hashtable class.
+ *
+ * @author Daniel Kleebinder, Christoph Rippel
+ */
 public class StockHashtable {
 
 	private final int THRESHOLD = 512;
 	private final int SIZE = 1000 + THRESHOLD;
 
-	private final SingleStock[] stockHT = new SingleStock[SIZE]; //Hashtable: Key = Name, Value = SingleStock
-	private final String[] nameAbbHT = new String[SIZE]; //Hashtable: Key = Abbreviation, Value = Name
+	private final SingleStock[] stockHT = new SingleStock[SIZE];
+	private final String[] nameAbbHT = new String[SIZE];
 
-	
-	//Calculate index for String keys, USE AT OWN RISK
-	@SuppressWarnings("unused")
-	private int calculateKey(String value) {
-	int result = 100;
-	result = Integer.parseInt(value)*result + 37;
-	return result;
-	}	
-	
 	//Save certain stock by using its name as key
 	@SuppressWarnings("unused")
 	public void putStockByName(String name, SingleStock st) {
 		int index = keyIndex(name);
-		if (stockHT[index].equals(null)) {
+		if (stockHT[index] == null) {
 			stockHT[index] = st;
 		} else {
 			//TODO: collision handling
@@ -35,11 +31,53 @@ public class StockHashtable {
 	@SuppressWarnings("unused")
 	public void putNameByAbbreviation(String name, String abb) {
 		int index = keyIndex(name);
-		if (nameAbbHT[index].isEmpty()) {
+		if (nameAbbHT[index] == null) {
 			nameAbbHT[index] = abb;
 		} else {
 			//TODO: collision handling
 		}
+	}
+
+	/**
+	 * Removes the single stock entry with the given name.
+	 *
+	 * @param name Name.
+	 * @return Removed item.
+	 */
+	public SingleStock removeStockByName(String name) {
+		int index = keyIndex(name);
+
+		SingleStock result = null;
+		SingleStock buffer;
+
+		while (stockHT[index] != null) {
+			result = stockHT[index];
+			if (name.equals(result.getName())) {
+				break;
+			} else {
+				// Use linear probing
+				index = (index + 1) % SIZE;
+			}
+		}
+
+		// Move all items after the deleted one
+		for (int j = (index + 1) % SIZE; stockHT[j] != null; j = (j + 1) % SIZE) {
+			buffer = stockHT[j];
+			stockHT[j] = null;
+			putStockByName(name, buffer);
+		}
+
+		return result;
+	}
+
+	/**
+	 * Removes the single stock entry with the given abbreviation.
+	 *
+	 * @param abb Abbreviation.
+	 * @return Removed item.
+	 */
+	public SingleStock removeStockByAbbreviation(String abb) {
+		return removeStockByName(getNameForAbbreviation(abb));
 	}
 
 	//Evil?  ¯\(ツ)/¯ 
@@ -59,5 +97,15 @@ public class StockHashtable {
 	 */
 	private int keyIndex(String str) {
 		return StringUtils.hashCode(str) % SIZE;
+	}
+
+	/**
+	 * Returns the name for the given abbreviation.
+	 *
+	 * @param abb Abbreviation.
+	 * @return Name.
+	 */
+	public String getNameForAbbreviation(String abb) {
+		return nameAbbHT[keyIndex(abb)];
 	}
 }
