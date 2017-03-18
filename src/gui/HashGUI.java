a80d3a46aa96c0f48f6dd6264028cb0d1d92f1c5
Daniel Kleebinder <daniel.kleebinder@gmx.net>
2017-03-18 11:11:36 +0100

    added and implemented "save", "save as..." and "load" functionallity to the GUI (+testing)

--- src/gui/HashGUI.java
+++ src/gui/HashGUI.java
@@ -14,20 +14,36 @@ import javax.swing.JTable;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JSeparator;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.xml.sax.SAXException;
+import util.HashtableIO;
 
 public class HashGUI extends JFrame {
 
 	private StockHashtable sht; //Source for table data
 	InputDialog id = new InputDialog(); //source for stockhashtable
 	DefaultTableModel dtm; //used for displaying stock
+
+	private File saveFile;
+	private File loadFile;
 
 	/**
 	 * Launch the application.
@@ -91,24 +107,56 @@ public class HashGUI extends JFrame {
 		mnFile.setMnemonic('f');
 
 		JMenuItem mntmSave = new JMenuItem("Save");
+		mntmSave.addActionListener((ActionEvent e) -> {
+			if (saveFile == null) {
+				saveFile = saveAsDialog();
+			}
+			if (saveFile == null) {
+				return;
+			}
+			if (!saveHashtable(saveFile)) {
+				JOptionPane.showConfirmDialog(null, "Cannot save file!", "Save", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+			}
+		});
+
+		JMenuItem mntmSaveAs = new JMenuItem("Save As ...");
+		mntmSaveAs.addActionListener((ActionEvent e) -> {
+			saveFile = saveAsDialog();
+			if (saveFile == null) {
+				return;
+			}
+			if (!saveHashtable(saveFile)) {
+				JOptionPane.showConfirmDialog(null, "Cannot save file!", "Save", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+			}
+		});
+
 		JMenuItem mntmLoad = new JMenuItem("Load");
-		JSeparator separator = new JSeparator();
+		mntmLoad.addActionListener((ActionEvent e) -> {
+			loadFile = loadDialog();
+			if (loadFile == null) {
+				return;
+			}
+			if (!loadHashtable(loadFile)) {
+				JOptionPane.showConfirmDialog(null, "Cannot load file!", "Load", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+			}
+		});
 
 		JMenuItem mntmExit = new JMenuItem("Exit");
-		mntmExit.addActionListener((ActionEvent evt) -> {
+		mntmExit.addActionListener((ActionEvent e) -> {
 			System.exit(0);
 		});
 
 		mnFile.add(mntmSave);
+		mnFile.add(mntmSaveAs);
 		mnFile.add(mntmLoad);
-		mnFile.add(separator);
+		mnFile.add(new JPopupMenu.Separator());
 		mnFile.add(mntmExit);
 
 		JMenu edit = new JMenu("Edit");
 
 		JMenuItem selectStock = new JMenuItem("Select Stock");
 		JMenuItem mntmImport = new JMenuItem("Import New Stock");
-		mntmImport.addActionListener((ActionEvent evt) -> {
+		mntmImport.addActionListener((ActionEvent e) -> {
 			InputDialog indi = new InputDialog();
 			indi.setModal(true);
 			indi.setVisible(true);
@@ -128,5 +176,65 @@ public class HashGUI extends JFrame {
 
 		getContentPane().add(menuBar, BorderLayout.NORTH);
 		getContentPane().add(centerContentPane, BorderLayout.CENTER);
+	}
+
+	/**
+	 * Shows the save as dialog and returns the file name.
+	 *
+	 * @return File name.
+	 */
+	private File saveAsDialog() {
+		JFileChooser jfc = new JFileChooser(saveFile);
+		jfc.setAcceptAllFileFilterUsed(false);
+		jfc.addChoosableFileFilter(new FileNameExtensionFilter("Stock Hashtable", "sht"));
+		if (jfc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+			return jfc.getSelectedFile();
+		}
+		return null;
+	}
+
+	/**
+	 * Shows the load dialog and returns the file name.
+	 *
+	 * @return File name.
+	 */
+	private File loadDialog() {
+		JFileChooser jfc = new JFileChooser(loadFile);
+		jfc.setAcceptAllFileFilterUsed(false);
+		jfc.addChoosableFileFilter(new FileNameExtensionFilter("Stock Hashtable", "sht"));
+		if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			return jfc.getSelectedFile();
+		}
+		return null;
+	}
+
+	/**
+	 * Saves the current state of the hashtable.
+	 *
+	 * @param file File.
+	 * @return True if successfully saved, otherwise false.
+	 */
+	private boolean saveHashtable(File file) {
+		try {
+			HashtableIO.exportTable(sht, new FileOutputStream(file));
+		} catch (FileNotFoundException | ParserConfigurationException | TransformerException ex) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Loads a hashtable.
+	 *
+	 * @param file File.
+	 * @return True if successfully loaded, otherwise false.
+	 */
+	private boolean loadHashtable(File file) {
+		try {
+			sht = HashtableIO.importTable(new FileInputStream(file));
+		} catch (IOException | SAXException | ParseException | ParserConfigurationException ex) {
+			return false;
+		}
+		return true;
 	}
 }
